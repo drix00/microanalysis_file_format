81d70e1723e881da8e5a15e2519065a1d265ad4a
Hendrix Demers <hendrix.demers@mail.mcgill.ca>
2020-02-27 18:33:03 -0500

    Update setup file
    Add pytest to requirement.
    Clean up install requirement.
    Fix style warning.

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ Setup script to use with pip for the project pyXraySpectrumFileFormat.
 """
 
 ###############################################################################
-# Copyright 2015 Hendrix Demers
+# Copyright 2020 Hendrix Demers
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -46,47 +46,37 @@ requirements = [
     'matplotlib',
     'scipy',
     'six',
-    'nose',
 ]
 
 test_requirements = [
-    'nose', 'coverage'
+    'nose', 'coverage', 'pytest', 'pytest-cov'
 ]
 
 readme_file_path = os.path.join(os.path.dirname(__file__), 'README.rst')
 long_description = open(readme_file_path).read() + '\n\n'
 
 setup(name="pySpectrumFileFormat",
-    version='0.1',
-    url='',
-    description="Project to read and write various x-ray spectrum file format.",
-	long_description=readme + '\n\n' + history,
-	author="Hendrix Demers",
-	author_email="hendrix.demers@mail.mcgill.ca",
-	license="Apache License, Version 2.0",
-	classifiers=['Development Status :: 5 - Production/Stable',
-		'Environment :: Console',
-		'Intended Audience :: Developers',
-		'Intended Audience :: Science/Research',
-        'License :: OSI Approved :: Apache Software License',
-		'Natural Language :: English',
-		'Programming Language :: Python',
-		'Operating System :: OS Independent',
-		'Topic :: Scientific/Engineering',],
-
-    packages=find_packages(),
-#    package_dir={'microanalysis_file_format':
-#                 'microanalysis_file_format'},
-    install_requires=requirements,
-    zip_safe=False,
-    keywords='microanalysis_file_format',
-
-	include_package_data=False, # Do not include test data
-
-    setup_requires=['nose', 'coverage'],
-
-    test_suite='nose.collector',
-    tests_require=test_requirements
-
-)
-
+      version='0.1',
+      url='',
+      description="Project to read and write various x-ray spectrum file format.",
+      long_description=readme + '\n\n' + history,
+      author="Hendrix Demers",
+      author_email="hendrix.demers@mail.mcgill.ca",
+      license="Apache License, Version 2.0",
+      classifiers=['Development Status :: 5 - Production/Stable',
+                   'Environment :: Console',
+                   'Intended Audience :: Developers',
+                   'Intended Audience :: Science/Research',
+                   'License :: OSI Approved :: Apache Software License',
+                   'Natural Language :: English',
+                   'Programming Language :: Python',
+                   'Operating System :: OS Independent',
+                   'Topic :: Scientific/Engineering', ],
+      packages=find_packages(),
+      install_requires=requirements,
+      zip_safe=False,
+      keywords='microanalysis_file_format',
+      include_package_data=False,  # Do not include test data
+      test_suite='nose.collector',
+      tests_require=test_requirements
+      )
